52ef36f73ee12265beff4b84d657d74f8cf7f217
Mikhail Onufriev <misha.onufriev@gmail.com>
2019-03-25 14:10:30 +0300

    First Homework

---
+++ variables/main.js
@@ -0,0 +1,24 @@
+let x = 2;
+let y = 22;
+let z = 0;
+let result;
+
+result = x * y + 5 * z + x - 1;
+
+console.log(result);
+
+
+let algebra = 4;
+let geography = 4;
+let physics = 4;
+let result2;
+
+result2 = (algebra + geography + physics) / 3;
+
+console.log(result2);
+
+
+let name = "Mikhail";
+let message = `Привет, мир! Меня зовут ${name}`;
+
+console.log(message);
