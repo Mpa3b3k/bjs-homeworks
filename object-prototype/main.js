830f6d82a626accf4884e8cfac447c69a4f9447a
Mikhail Onufriev <misha.onufriev@gmail.com>
2019-04-10 21:53:27 +0300

    commit

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function initCheckBirthday() {
     const birthday = document.getElementById('birthday').value;
 
@@ -7,7 +9,19 @@ function initCheckBirthday() {
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №1 писать здесь
+    let now = new Date();
+	let last = new Date(birthday);
+	let checkedResult = +now - (+last);
+
+
+	if (checkedResult >= 568080000000) {
+		console.log("Age >= 18");
+		return true;
+	}
+	else {
+		console.log("Fee, teenager");
+		return false;
+	}
 }
 
 function initPrintAnimalSound() {
@@ -21,7 +35,13 @@ function initPrintAnimalSound() {
 }
 
 function getAnimalSound(animal) {
-    // код для задачи №2 писать здесь
+    let sound = animal.sound;
+    if (sound != undefined) {
+    	return sound;
+    }
+    else {
+    	return null;
+    }
 }
 
 function initCalculateStatement() {
@@ -35,5 +55,17 @@ function initCalculateStatement() {
 }
 
 function getAverageMark(marks) {
-    // код для задачи №3 писать здесь
+
+	let summ = 0;
+	let i = 0;
+
+    for (i = 0; i < marks.length; i++) {
+    	summ += +marks[i];
+    }
+
+    let average = summ / marks.length;
+	
+	let roundedAverage = Math.round(average);
+
+	return roundedAverage;
 }
