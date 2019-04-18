06cec264220ab7508006c47b8bca137f067b50fb
Mikhail Onufriev <misha.onufriev@gmail.com>
2019-04-18 15:54:29 +0300

    assync

---
+++ async/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let now = new Date();
+
+function setAlarm(time, callback) {
+	
+	let timeTo = new Date();
+
+
+
+	return callback;
+}
+
+function setDailyRhythm(wakeUpTime, bedTime) {
+	
+}
+
+setInterval(setAlarm, 1000, now, setDailyRhythm);
